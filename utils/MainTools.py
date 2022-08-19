fccc4788139488e1c8bb098c6afb7f80252570d1
Sonya Fatholahi <sonyadream@protonmail.com>
2022-08-19 19:30:55 +0430

    added more functionality and added Download Class

--- utils/MainTools.py
+++ utils/MainTools.py
@@ -5,47 +5,17 @@ import sys
 import subprocess
 
 
-def sort_resolutions(link):
-    "sort stream order"
-    global video_resolutions
-    global videos
-
-    # Title of The Video
-    print(link.title)
-
-    # Thumbnail Image and Description
-    print(link.thumbnail_url)
-    print(f"\n{link.description}")
-
-    video_resolutions = []
-    videos = []
-
-    for stream in link.streams.order_by('resolution'):
-        video_resolutions.append(stream)
-        videos.append(stream)
-
-    return video_resolutions, videos
-
-
-def getcover(link):
-    "get cover of url link"
-    global temp
-    img_data = requests.get(link.thumbnail_url).content
-    temp = tempfile.NamedTemporaryFile()
-    with open(f'{temp.name}', 'wb') as handler:
-        handler.write(img_data)
-
-
-def mediatype(link,type,destination):
-    "media type selection and downloading media"
-
-    global video
-    global out
+class Download():
+    def __init__(self,destination):
+        "media type selection and downloading media"
+        self.destination = destination
+        if not destination:
+            destination = f'{sys.path[0]}/'
     
-    if not destination:
-        destination = f'{sys.path[0]}/'
-
-    if type == 'video':
+    def video(self,link):
+        global videos
+        global out
+    
         while True:
             # Looping through the video_resolutions list to be displayed on the screen for user selection...
             i = 1
@@ -61,31 +31,60 @@ def mediatype(link,type,destination):
                 print(f"downloading {resolution_to_download}")
 
                 # command for downloading the video
-                out = videos[choice - 1].download(output_path=destination)
+                out = videos[choice - 1].download(output_path=self.destination)
 
                 break
             else:
                 print("Invalid choice!!\n\n")
+                
+    def audio(self,link):
+            video = link.streams.filter(only_audio=True).first()
+            out = video.download(output_path=self.destination)
+            try:
+                base, ext = os.path.splitext(out)
+                os.rename(out, base)
 
-    elif type == 'audio':
-        video = link.streams.filter(only_audio=True).first()
-        out = video.download(output_path=destination)
-        try:
-            base, ext = os.path.splitext(out)
-            os.rename(out, base)
+                cmd = f"ffmpeg -y -loop 1 -i '{temp.name}' -i '{os.path.realpath(base)}' -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest 'out.mp4'"
+                subprocess.check_output(cmd, shell=True)
+                subprocess.call(cmd, shell=True)
 
-            cmd = f"ffmpeg -y -loop 1 -i '{temp.name}' -i '{os.path.realpath(base)}' -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest 'out.mp4'"
-            subprocess.check_output(cmd, shell=True)
-            subprocess.call(cmd, shell=True)
+                os.rename('out.mp4', base + '.mp3')
+                os.remove(base)
 
-            os.rename('out.mp4', base + '.mp3')
-            os.remove(base)
+                temp.close()
+            except subprocess.CalledProcessError:
+                print('ffmpeg are not installed')
+                exit(1)
+    
+    def sort_resolutions(link):
+        "sort stream order"
+        global video_resolutions
+        global videos
 
-            temp.close()
-        except subprocess.CalledProcessError:
-            print('ffmpeg are not installed')
-            exit(1)
+        # Title of The Video
+        print(link.title)
 
-    else:
-        print("Invalid selection.")
-        exit(1)
+        # Thumbnail Image and Description
+        print(link.thumbnail_url)
+
+        video_resolutions = []
+        videos = []
+
+        for stream in link.streams.order_by('resolution'):
+            video_resolutions.append(stream)
+            videos.append(stream)
+
+        return video_resolutions, videos
+
+
+    def getcover(link):
+        "get cover of url link"
+        global temp
+        img_data = requests.get(link.thumbnail_url).content
+        temp = tempfile.NamedTemporaryFile()
+        with open(f'{temp.name}', 'wb') as handler:
+            handler.write(img_data)
+
+    def show_description(link):
+        "show video description"
+        return (f"\n{link.description}")
--- ytbot.py
+++ ytbot.py
@@ -3,8 +3,10 @@
 from pytube import YouTube
 from pytube.cli import on_progress
 from argparse import ArgumentParser
+
+# utils library
 from utils.CheckLink import link_is_valid
-from utils.MainTools import sort_resolutions, mediatype, getcover
+from utils.MainTools import Download
 from utils.InterruptHandler import sigint_handler
 from utils.InterruptHandler import signal
 
@@ -16,6 +18,11 @@ parser.add_argument('link')
 parser.add_argument('--video','-v',action='store_true',help='download URL with video')
 parser.add_argument('--audio','-a',action='store_true',help='download URL with only audio')
 parser.add_argument('--dir','-d',action='store',type=str,help='set destination')
+
+optionalparser = parser.add_argument_group( 'optional options' )
+optionalparser.add_argument('--description','-desc',action='store_true',help='download URL with only audio')
+
+
 argv_parsed = parser.parse_args()
 
 video_link = argv_parsed.link
@@ -25,17 +32,21 @@ if not link_is_valid:
     print("Enter valid video link")
     quit()
 
+dl = Download(destination)
 link = YouTube(str(video_link), on_progress_callback=on_progress)
 
-sort_resolutions(link)
+Download.sort_resolutions(link)
+
+if argv_parsed.description:
+    print(Download.show_description(link))
+
 if argv_parsed.video:
-    mediatype(link,'video',destination)
+    dl.video(link)
 elif argv_parsed.audio:
-    getcover(link)
-    mediatype(link,'audio',destination)
+    Download.getcover(link)
+    dl.audio(link)
 else:
     print('set mediatype --video for video --audio for only audio')
     exit()
-
 
 print(f'{link.title} Downloaded.')
