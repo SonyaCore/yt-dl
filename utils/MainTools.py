ee92c0ab1e3dd6d2228d7b45594e1fe0f43a7947
Sonya <sonyacore@protonmail.com>
2022-12-23 12:32:29 +0330

    fixed playlist stream

--- utils/MainTools.py
+++ utils/MainTools.py
@@ -62,7 +62,7 @@ class Download():
         #      print(url)
         for video in link.videos:
             print(video.title)
-            video.streamsvideo.streams.\
+            video.streams.\
             filter(type='video', progressive=True, file_extension='mp4').\
             order_by('resolution').\
             desc().\
